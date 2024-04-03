b06fefa549d7843bc0d371db9e09afa8a3b64984
GITA <jgitanjali592@gmail.com>
2024-04-03 11:04:19 +0530

    this code for navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import {Footer , Blog , Possibility , Features , WhatGPT3 , Header } from './Containers';
 
-import{CTA , Brand , Navbar } from './Components';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './Containers';
+import { CTA, Brand, Navbar } from './Components';
 
-const App = () => {
-  return (
-    <div className=' App'>
-     <div className='gradient_bg'>
-      <Navbar/>
-      <Header/>
-     </div>
-     <Brand/>
-     <WhatGPT3/>
-     <Features/>
-     <Possibility/>
-     <CTA/>
-     <Blog/>
-     <Footer/>
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  )
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
