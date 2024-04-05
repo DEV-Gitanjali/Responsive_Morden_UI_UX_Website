8e97df1a457ca5060dc39a4894a87586a9f276ff
GITA <jgitanjali592@gmail.com>
2024-04-05 10:17:03 +0530

    this code for blog & article

--- src/Components/article/Article.jsx
+++ src/Components/article/Article.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
-  return (
-    <div>
-      article
+const Article = ({imgUrl, date , text}) => (
+  <div className='gpt3__blog-container_article'>
+    <div className='gpt3__blog-container_article-image'>
+      <img src={imgUrl} alt="blog" />
     </div>
-  )
-}
+
+<div className='gpt3__blog-container_article-content'>
+  <p>{date}</p>
+  <h3>{text}</h3>
+</div>
+
+  <p>Read full Article</p>
+  </div>
+)
+  
 
 export default Article
